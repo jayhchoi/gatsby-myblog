197a510949fad324395feb5e4bfa389128114635
JayHChoi <jaychoi1619@gmail.com>
2020-04-15 15:36:19 -0400

    change num of posts per page

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
 		'MBA 학생 | University of Virginia에서 MBA와 MS Data Science 학위를 이수 중입니다. 미국 생활, MBA, 데이터 사이언스 등 여러 이야기들을 나누고 싶습니다.',
 	copyright: 'Copyright © 최혁재의 블로그 2020. All rights reserved.',
 	disqusShortname: '',
-	postsPerPage: 8,
+	postsPerPage: 5,
 	googleAnalyticsId: 'UA-163695775-1',
 	menu: [
 		{
@@ -17,11 +17,11 @@ module.exports = {
 		{
 			label: 'About me',
 			path: '/pages/about',
-		},
-		{
-			label: 'Contact me',
-			path: '/pages/contacts',
-		},
+		}, //,
+		// {
+		// 	label: 'Contact me',
+		// 	path: '/pages/contacts',
+		// },
 	],
 	author: {
 		name: '최혁재 (Jay Choi)',
